396664b8b78e59fdc55e96f3c5f10e6e718cef1f
vankhang49 <dvkhang49@gmail.com>
2024-01-06 22:23:48 +0700

    up

--- websiteLayoutDesign_sesson7/bai_tap/cartoon/cartoon.js
+++ websiteLayoutDesign_sesson7/bai_tap/cartoon/cartoon.js
@@ -1,7 +1,25 @@
 const playMV = document.querySelectorAll(".list-film .film");
+
 playMV.forEach(function (item, index) {
     item.addEventListener("click", function () {
         console.log(index)
-        document.getElementById("saw").src = "./videos/Tập " + (index + 1) + ".mp4";
+        let srcFilm;
+        switch (index){
+            case 0:
+                srcFilm = "https://youtu.be/AeaD3Q-bFjU?si=tNwLapIaXgmpqqgD";
+                break;
+            case 1:
+                srcFilm = "https://youtu.be/mOUQwVUO4q0?si=vmZA8-UgJT-dDzfZ";
+                break;
+            case 2:
+                srcFilm = "https://youtu.be/tzv86xibN6s?si=Ui6gYaA161TVgJ7M";
+                break;
+            case 3:
+                srcFilm = "https://youtu.be/puHNx_44koE?si=kKjo_ueMBehvFPl-";
+                break;
+            default:
+                srcFilm = "https://youtu.be/AeaD3Q-bFjU?si=tNwLapIaXgmpqqgD";
+        }
+        document.getElementById("saw").src = srcFilm;
     })
 })
