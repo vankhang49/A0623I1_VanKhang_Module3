8ad69c21ea86e1f9afd71c778a139e88a5fe2233
vankhang49 <dvkhang49@gmail.com>
2024-01-17 17:48:31 +0700

    up

---
+++ JDBC_&_CRUD_sesson12/User_management/user-management/src/main/java/com/example/usermanagement/repository/IUserRepository.java
@@ -0,0 +1,18 @@
+package com.example.usermanagement.repository;
+
+import com.example.usermanagement.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserRepository {
+    public void insertUser(User user) throws SQLException;
+
+    public User selectUser(int id);
+
+    public List<User> selectAllUsers();
+
+    public boolean deleteUser(int id) throws SQLException;
+
+    public boolean updateUser(User user) throws SQLException;
+}
